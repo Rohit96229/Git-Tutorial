e4a36e15a55e3850bf15a962c681a881175ce11b
Rohit96229 <rohit.thoughtwin@gmail.com>
2020-04-03 19:59:30 +0530

    initial change

--- src/App.js
+++ src/App.js
@@ -31,3 +31,75 @@ class App extends Component {
 }
 
 export default App;
+// import React from "react";
+// import "./App.css";
+// import Router from "./Router";
+// import { NavLink } from "react-router-dom";
+// import {
+//   /*BrowserRouter as Router, */ Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+
+// const routes = [
+//   {
+//     path: "/",
+//     exact: true,
+//     sidebar: () => <div>home!</div>,
+//     main: () => <h2>Home</h2>
+//   },
+//   {
+//     path: "/cart",
+//     sidebar: () => <div>cart!</div>,
+//     main: () => <h2>cart</h2>
+//   }
+// ];
+
+// export default function SidebarExample() {
+//   return (
+//     <Router>
+//       <div style={{ display: "flex" }}>
+//         <div
+//           style={{
+//             padding: "10px",
+//             width: "40%",
+//             background: "#f0f0f0"
+//           }}
+//         >
+//           <ul style={{ listStyleType: "none", padding: 0 }}>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/cart">cart</Link>
+//             </li>
+//           </ul>
+
+//           <Switch>
+//             {routes.map((route, index) => (
+//               <Route
+//                 key={index}
+//                 path={route.path}
+//                 exact={route.exact}
+//                 children={<route.sidebar />}
+//               />
+//             ))}
+//           </Switch>
+//         </div>
+
+//         <div style={{ flex: 1, padding: "10px" }}>
+//           <Switch>
+//             {routes.map((route, index) => (
+//               <Route
+//                 key={index}
+//                 path={route.path}
+//                 exact={route.exact}
+//                 children={<route.main />}
+//               />
+//             ))}
+//           </Switch>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
--- src/config/store.js
+++ src/config/store.js
@@ -1,5 +1,6 @@
 import { createStore, combineReducers } from "redux";
-import cartReducer from "../features/cart/reducer";
+//import cartReducer from "../features/cart/reducer";
+import cartReducer from "./reducer";
 
 const rootReducer = combineReducers({
   cart: cartReducer
